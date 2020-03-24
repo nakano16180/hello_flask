939bb51b57f33339e016ed41092a55741b8f9022
nakano16180 <nakano16180@gmail.com>
2020-03-24 22:18:48 +0900

    add sample pages

--- hello_flask.py
+++ hello_flask.py
@@ -19,6 +19,23 @@ def hello_world_ja():
 def index():
     return render_template('index.html') #/indexにアクセスが来たらtemplates内のindex.htmlが開きます
 
+@app.route("/string")
+def string():
+    message = 'sample_string'
+    return render_template('string.html', message=message)
+
+@app.route("/list")
+def list():
+    num_list = [1, 2, 3, 4, 5]
+    return render_template('list.html', message=num_list)
+
+@app.route("/dictionary")
+def dictionary():
+    my_dic = {}
+    my_dic['name'] = 'fugafuga'
+    my_dic['univ'] = 'hogehoge University'
+    return render_template('dictionary.html', message=my_dic)
+
 if __name__ == '__main__':
     # webサーバー立ち上げ
     app.run(host='0.0.0.0', port=port)
