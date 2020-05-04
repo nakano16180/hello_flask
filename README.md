b29bd4bc53bea988261ea8a46643e48783b4a729
nakano16180 <nakano16180@gmail.com>
2020-05-04 22:06:08 +0900

    add api

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ $ pipenv install
 ```
 $ pipenv run python hello_flask.py
 ```
+
+### api
+
+```
+$ curl http://localhost:5000/api/test
+$ curl -X POST http://localhost:5000/api/test/1234
+```
+
+json
+```
+$ curl http://localhost:5000/api
+$ curl -X POST -H 'Accept:application/json' -H 'Content-Type:application/json' http://localhost:5000/api -d '{"data":"hoge"}'
+```
--- hello_flask.py
+++ hello_flask.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 import os
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 # Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
@@ -35,6 +36,27 @@ def dictionary():
     my_dic['name'] = 'fugafuga'
     my_dic['univ'] = 'hogehoge University'
     return render_template('dictionary.html', message=my_dic)
+
+@app.route('/api/test')
+def get_foobar():
+    return 'you got\n'
+
+@app.route('/api/test/<int:baz>', methods=['POST'])
+def post_foobar(baz):
+    return 'you posted {}\n'.format(baz)
+
+@app.route('/api', methods=['GET'])
+def get_json_from_dictionary():
+    dic = {
+        'foo': 'bar',
+        'ほげ': 'ふが'
+    }
+    return jsonify(dic)  # JSONをレスポンス
+
+@app.route('/api', methods=['POST'])
+def post_json():
+    json = request.get_json()  # POSTされたJSONを取得
+    return jsonify(json)  # JSONをレスポンス
 
 if __name__ == '__main__':
     # webサーバー立ち上げ
